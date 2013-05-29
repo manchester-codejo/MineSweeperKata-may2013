7f301125951dc22765ce692e654925fe435e8a5d
Gemma Cameron <rubygem@Gemmas-MacBook-Pro.local>
2013-05-29 09:33:17 +0100

    replacing the tabs with double space to appease the pythonists

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,62 +1,62 @@
 require 'test/unit'
 
 class TestDisplay < Test::Unit::TestCase
-	def test_see_output_of_empty_one_by_one_grid
-		mine_field = MineField.new(self)
-		mine_field.solve
-		assert_equal EmptyCell.new.value, @grid_output
-	end
+  def test_see_output_of_empty_one_by_one_grid
+    mine_field = MineField.new(self)
+    mine_field.solve
+    assert_equal EmptyCell.new.value, @grid_output
+  end
 
-	def test_bob
-		mine_field = MineField.new(self)
-		mine_field.solve
-		#assert @grid_output.equal? EmptyCell.new
-	end
+  def test_bob
+    mine_field = MineField.new(self)
+    mine_field.solve
+    #assert @grid_output.equal? EmptyCell.new
+  end
 
-	def test_output_of_one_by_one_grid_with_mother_fudging_bomb
-		mine_field = MineField.new(self)
-		mine_field.plant_a_bomb
-		mine_field.solve
-		assert_equal BombCell.new.value, @grid_output		
-	end
+  def test_output_of_one_by_one_grid_with_mother_fudging_bomb
+    mine_field = MineField.new(self)
+    mine_field.plant_a_bomb
+    mine_field.solve
+    assert_equal BombCell.new.value, @grid_output    
+  end
 
-	def output(grid)
-		@grid_output = grid
-	end
+  def output(grid)
+    @grid_output = grid
+  end
 end
 
 class EmptyCell
-	EMPTY = ' '
+  EMPTY = ' '
 
-	def value
-		EMPTY
-	end
+  def value
+    EMPTY
+  end
 
-	def equal?(other)
-		return true
-	end
+  def equal?(other)
+    return true
+  end
 end
 
 class BombCell
-	BOMB = '*'
+  BOMB = '*'
 
-	def value
-		BOMB
-	end
+  def value
+    BOMB
+  end
 end
 
 
 class MineField
-	def initialize(display)
-		@display = display
-		@grid = EmptyCell.new
-	end
+  def initialize(display)
+    @display = display
+    @grid = EmptyCell.new
+  end
 
-	def solve
-		@display.output(@grid.value)
-	end
+  def solve
+    @display.output(@grid.value)
+  end
 
-	def plant_a_bomb
-		@grid = BombCell.new	
-	end
+  def plant_a_bomb
+    @grid = BombCell.new  
+  end
 end
